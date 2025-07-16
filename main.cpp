8cf7bc50603ea6029fe2d8d394d93013bbadcfad
Marwan-AB <Marwan-Aboelnour@aucegypt.edu>
2025-07-16 17:11:16 +0300

    added home page

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "Authentication.h"
 #include <fstream>
 #include <sstream>
+#include <filesystem>
+namespace fs = std::filesystem;
 using namespace std;
 int main() {
     crow::SimpleApp app;
@@ -14,7 +16,23 @@ int main() {
     buffer << file.rdbuf();
     return crow::response{buffer.str()};
 });
+  CROW_ROUTE(app, "/home")
+([] {
+    ifstream file("UI/home.html");
+    if (!file.is_open()) {
+        cerr << "Failed to open UI/home.html\n";
+        return crow::response(500, "Home page not found");
+    }
 
+    stringstream buffer;
+    buffer << file.rdbuf();
+
+    crow::response res;
+    res.code = 200;
+    res.set_header("Content-Type", "text/html");
+    res.body = buffer.str();
+    return res;
+});
     CROW_ROUTE(app, "/signup").methods("POST"_method)([&auth](const crow::request& req) {
         auto body = crow::json::load(req.body);
         if (!body) return crow::response(400, "Invalid JSON");
@@ -28,23 +46,24 @@ int main() {
             return crow::response(409, "Username already exists");
     });
 
-    CROW_ROUTE(app, "/login").methods("POST"_method)([&auth](const crow::request& req) {
-        auto body = crow::json::load(req.body);
-        if (!body) return crow::response(400, "Invalid JSON");
+    CROW_ROUTE(app, "/login").methods("POST"_method)([&auth](const crow::request& req){
+    auto body = crow::json::load(req.body);
+    if (!body) return crow::response(400, "Invalid JSON");
 
-        string uname = body["username"].s();
-        string pwd = body["password"].s();
+    string uname = body["username"].s();
+    string pwd = body["password"].s();
 
-        string sid = auth.login(uname, pwd);
-        if (!sid.empty()) {
-            crow::json::wvalue res;
-            res["message"] = "Login successful";
-            res["sessionID"] = sid;
-            return crow::response{res};
-        } else {
-            return crow::response(401, "Invalid credentials");
-        }
-    });
+    string sid = auth.login(uname, pwd);
+    if (!sid.empty()) {
+        crow::response res;
+        res.code = 200;
+        res.set_header("Content-Type", "application/json");
+        res.body = R"({"status":"ok"})";  // tell JavaScript to redirect
+        return res;
+    } else {
+        return crow::response(401, "Invalid credentials");
+    }
+});
 
     app.port(18080).multithreaded().run();
 }
